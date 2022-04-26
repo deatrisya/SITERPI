6e0ab6db4bdca60bb398660e8efb9393adc58eb0
deatrisya <mirelldee47@gmail.com>
2022-04-26 20:33:44 +0700

    modify nav item

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -25,23 +25,6 @@
               <span class="menu-title">Dashboard</span>
             </a>
           </li>
-          {{-- <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-              <i class="menu-icon mdi mdi-content-copy"></i>
-              <span class="menu-title">Hewan Ternak</span>
-              <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="ui-basic">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item">
-                  <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="pages/ui-features/typography.html">Typography</a>
-                </li>
-              </ul>
-            </div>
-          </li> --}}
           <li class="nav-item">
             <a class="nav-link" href="{{route('user.index')}}">
               <i class="menu-icon mdi mdi-account"></i>
@@ -50,32 +33,71 @@
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">
-              <i class="menu-icon mdi mdi-donkey"></i>
-              <span class="menu-title">Hewan Ternak</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
               <i class="menu-icon mdi mdi-account-multiple-outline"></i>
-              <span class="menu-title">Karyawan</span>
+              <span class="menu-title">Data Pegawai</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
-              <i class="menu-icon mdi mdi-cup"></i>
-              <span class="menu-title">Sarana Produksi</span>
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <i class="menu-icon mdi mdi-cow"></i>
+              <span class="menu-title">Master Sapi</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item">
+                  <a class="nav-link" href="pages/ui-features/buttons.html">Jenis Sapi</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="pages/ui-features/typography.html">Data Sapi</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic1" aria-expanded="false" aria-controls="ui-basic1">
+              <i class="menu-icon mdi mdi-corn"></i>
+              <span class="menu-title">Master Pakan</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic1">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item">
+                  <a class="nav-link" href="pages/ui-features/buttons.html">Jenis Pakan</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="pages/ui-features/typography.html">Riwayat Pakan</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic2" aria-expanded="false" aria-controls="ui-basic2">
+              <i class="menu-icon mdi mdi-medical-bag"></i>
+              <span class="menu-title">Master Obat</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic2">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item">
+                  <a class="nav-link" href="pages/ui-features/buttons.html">Jenis Obat</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="pages/ui-features/typography.html">Riwayat Obat</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" >
+              <i class="menu-icon mdi mdi-clipboard-text"></i>
+              <span class="menu-title">Transaksi</span>
             </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">
               <i class="menu-icon mdi mdi-cash-multiple"></i>
               <span class="menu-title">Keuangan</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" >
-              <i class="menu-icon mdi mdi-clipboard-text"></i>
-              <span class="menu-title">Transaksi</span>
             </a>
           </li>
         </ul>
