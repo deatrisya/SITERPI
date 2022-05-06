d9b4d69553e237bc73e7fb46fe3f0169317f7911
deatrisya <mirelldee47@gmail.com>
2022-05-06 17:24:55 +0700

    modify sidebar content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,83 +1,29 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{asset('admin/vendors/iconfonts/mdi/css/materialdesignicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('admin/vendors/css/vendor.bundle.base.css')}}">
+    <link rel="stylesheet" href="{{asset('admin/vendors/css/vendor.bundle.addons.css')}}">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('admin/css/style.css')}}">
+    <link rel="shorcut icon" href="{{asset('admin/images/logo-siterpi.png')}}">
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+<body class="content-wrapper">
+    <div class="container">
+        @yield('content')
     </div>
+
+    <script src="{{asset('admin/vendors/js/vendor.bundle.base.js')}}"></script>
+    <script src="{{asset('admin/vendors/js/vendor.bundle.addons.js')}}"></script>
+    <script src="{{asset('admin/js/off-canvas.js')}}"></script>
+    <script src="{{asset('admin/js/misc.js')}}"></script>
+    <script src="{{asset('admin/js/dashboard.js')}}"></script>
 </body>
+
 </html>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,104 +1,106 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
-        <ul class="nav">
-          <li class="nav-item nav-profile">
+    <ul class="nav">
+        <li class="nav-item nav-profile">
             <div class="nav-link">
-              <div class="user-wrapper">
-                <div class="profile-image">
-                  <img src="{{asset('admin/images/faces/face1.jpg')}}" alt="profile image">
+                <div class="user-wrapper">
+                    <div class="profile-image">
+                        <img src="{{asset('admin/images/faces/face1.jpg')}}" alt="profile image">
+                    </div>
+                    <div class="text-wrapper">
+                        <p class="profile-name">{{Auth()->user()->nama}}</p>
+                        <div>
+                            <small class="designation text-muted">{{Auth()->user()->jabatan}}</small>
+                            <span class="status-indicator online"></span>
+                        </div>
+                    </div>
                 </div>
-                <div class="text-wrapper">
-                  <p class="profile-name">{{Auth()->user()->nama}}</p>
-                  <div>
-                    <small class="designation text-muted">{{Auth()->user()->jabatan}}</small>
-                    <span class="status-indicator online"></span>
-                  </div>
-                </div>
-              </div>
-              {{-- <button class="btn btn-success btn-block">New Project
+                {{-- <button class="btn btn-success btn-block">New Project
                 <i class="mdi mdi-plus"></i>
               </button> --}}
             </div>
-          </li>
-          <li class="nav-item">
+        </li>
+        <li class="nav-item">
             <a class="nav-link" href="{{route('home')}}">
-              <i class="menu-icon mdi mdi-television"></i>
-              <span class="menu-title">Dashboard</span>
+                <i class="menu-icon mdi mdi-television"></i>
+                <span class="menu-title">Dashboard</span>
             </a>
-          </li>
-          <li class="nav-item">
+        </li>
+        <li class="nav-item">
             <a class="nav-link" href="{{route('user.index')}}">
-              <i class="menu-icon mdi mdi-account"></i>
-              <span class="menu-title">Data User</span>
+                <i class="menu-icon mdi mdi-account"></i>
+                <span class="menu-title">Data User</span>
             </a>
-          </li>
-          <li class="nav-item">
+        </li>
+        <li class="nav-item">
             <a class="nav-link" href="#">
-              <i class="menu-icon mdi mdi-account-multiple-outline"></i>
-              <span class="menu-title">Data Pegawai</span>
+                <i class="menu-icon mdi mdi-account-multiple-outline"></i>
+                <span class="menu-title">Data Pegawai</span>
             </a>
-          </li>
-          <li class="nav-item">
+        </li>
+        <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-              <i class="menu-icon mdi mdi-cow"></i>
-              <span class="menu-title">Master Sapi</span>
-              <i class="menu-arrow"></i>
+                <i class="menu-icon mdi mdi-cow"></i>
+                <span class="menu-title">Master Sapi</span>
+                <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="ui-basic">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item">
-                  <a class="nav-link" href="pages/ui-features/buttons.html">Jenis Sapi</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="pages/ui-features/typography.html">Data Sapi</a>
-                </li>
-              </ul>
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('jenissapi.index')}}">Jenis Sapi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="pages/ui-features/typography.html">Data Sapi</a>
+                    </li>
+                </ul>
             </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#ui-basic1" aria-expanded="false" aria-controls="ui-basic1">
-              <i class="menu-icon mdi mdi-corn"></i>
-              <span class="menu-title">Master Pakan</span>
-              <i class="menu-arrow"></i>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic1" aria-expanded="false"
+                aria-controls="ui-basic1">
+                <i class="menu-icon mdi mdi-corn"></i>
+                <span class="menu-title">Master Pakan</span>
+                <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="ui-basic1">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item">
-                  <a class="nav-link" href="pages/ui-features/buttons.html">Jenis Pakan</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="pages/ui-features/typography.html">Riwayat Pakan</a>
-                </li>
-              </ul>
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item">
+                        <a class="nav-link" href="pages/ui-features/buttons.html">Jenis Pakan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="pages/ui-features/typography.html">Riwayat Pakan</a>
+                    </li>
+                </ul>
             </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#ui-basic2" aria-expanded="false" aria-controls="ui-basic2">
-              <i class="menu-icon mdi mdi-medical-bag"></i>
-              <span class="menu-title">Master Obat</span>
-              <i class="menu-arrow"></i>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic2" aria-expanded="false"
+                aria-controls="ui-basic2">
+                <i class="menu-icon mdi mdi-medical-bag"></i>
+                <span class="menu-title">Master Obat</span>
+                <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="ui-basic2">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item">
-                  <a class="nav-link" href="pages/ui-features/buttons.html">Jenis Obat</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="pages/ui-features/typography.html">Riwayat Obat</a>
-                </li>
-              </ul>
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item">
+                        <a class="nav-link" href="pages/ui-features/buttons.html">Jenis Obat</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="pages/ui-features/typography.html">Riwayat Obat</a>
+                    </li>
+                </ul>
             </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" >
-              <i class="menu-icon mdi mdi-clipboard-text"></i>
-              <span class="menu-title">Transaksi</span>
-            </a>
-          </li>
-          <li class="nav-item">
+        </li>
+        <li class="nav-item">
             <a class="nav-link" href="#">
-              <i class="menu-icon mdi mdi-cash-multiple"></i>
-              <span class="menu-title">Keuangan</span>
+                <i class="menu-icon mdi mdi-clipboard-text"></i>
+                <span class="menu-title">Transaksi</span>
             </a>
-          </li>
-        </ul>
-      </nav>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="menu-icon mdi mdi-cash-multiple"></i>
+                <span class="menu-title">Keuangan</span>
+            </a>
+        </li>
+    </ul>
+</nav>
