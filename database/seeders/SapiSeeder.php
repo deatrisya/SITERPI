f5667bd648656bce70fd88ee6c22e8312190e16b
deatrisya <mirelldee47@gmail.com>
2022-05-08 21:58:06 +0700

    Create Sapi Seeder

---
+++ database/seeders/SapiSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SapiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $dataItem = [
+            [
+                'jenissapi_id' => 1,
+                'nis' => 'S0001',
+                'tanggal_lahir' => '2021-01-01',
+                'status' => 'Terjual',
+                'jenis_kelamin' => 'Jantan',
+                'bobot' => 7,
+                'harga' => 500000,
+                'kondisi' => 'Sehat',
+                'keterangan' => null
+            ],
+            [
+                'jenissapi_id' => 1,
+                'nis' => 'S0002',
+                'tanggal_lahir' => '2021-02-02',
+                'status' => 'Belum Terjual',
+                'jenis_kelamin' => 'Jantan',
+                'bobot' => 7,
+                'harga' => 500000,
+                'kondisi' => 'Sehat',
+                'keterangan' => null
+            ]
+        ];
+        DB::table('sapis')->insert($dataItem);
+    }
+}
