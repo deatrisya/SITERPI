bb0f27f6bc3b65bebf128dc07408948d79a5bc87
deatrisya <mirelldee47@gmail.com>
2022-05-19 12:18:00 +0700

    Membuat view CRUD Transaksi

---
+++ resources/views/transaksi/transaksiCreate.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.app')
+@section('title')
+    Tambah Data Transaksi
+@endsection
+@section('content')
+    <div class="mt-5 col-md-8 mx-auto">
+        <div class="card ">
+            <h5 class="card-header bg-orange text-white">Tambah Data Transaksi</h5>
+            <div class="card-body">
+              @if ($errors->any())
+              <div class="alert alert-danger">
+                <strong>Whoops!</strong>There were some problems with your input.<br><br>
+                <ul>
+                    @foreach ($errors->all as $error)
+                    <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+              </div>
+
+              @endif
+              <form method="POST" action="{{route('transaksi.store')}}">
+                @csrf
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">ID Sapi</label>
+                            <select class="form-control" id="sapi_id" name="sapi_id" required>
+                                <option value="">--Pilih ID Sapi--</option>
+                                @foreach ($sapi as $data)
+                                    <option value="{{$data->id}}">{{$data->nis}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Status Transaksi</label>
+                            <input type="text" class="form-control" name="status_transaksi" value="Jual" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Harga</label>
+                            <input type="number" class="form-control" id="harga" name="harga"></input>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-orange">Submit</button>
+                <a class="btn btn-secondary" href="{{ route('transaksi.index')}}">Cancel</a>
+            </form>
+
+         </div>
+        </div>
+    </div>
+@endsection
+
+@section('js')
+<script>
+    $(document).ready(function(){
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+            }
+        });
+        $('select#sapi_id').on('change',function(e){
+            var selected_sapi = $(this).children("option:selected").val();
+            $.ajax({
+                type:"GET",
+                dataType:"json",
+                url:'/getTransaksi/'+selected_sapi,
+                success:function(response){
+                    console.log(response);
+                    $('#harga').val(response.harga)
+                }
+            })
+        });
+    });
+
+
+</script>
+@endsection
---
+++ resources/views/transaksi/transaksiIndex.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.adminLayout')
+@section('title')
+    Data Transaksi | SITERPI
+@endsection
+@section('content')
+<div class="col-lg-12 grid-martin stretch-card">
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">Data Transaksi</h4>
+            <div class="button">
+               <div class="row">
+                   <div class="col-md-6">
+                    <div class="float-left">
+                        <a class="btn btn-orange" href="{{route('transaksi.create')}}">+ Tambah Data</a>
+                    </div>
+                   </div>
+                   <div class="col-md-6">
+                    <div class="float-right">
+                        <form class="form-inline my-2 my-lg-0" action="{{url()->current()}}" method="GET">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="keyword" value="{{request('keyword')}}">
+                            <button class="btn btn-icons btn-orange" type="submit"><i class="mdi mdi-magnify"></i></button>
+                        </form>
+                   </div>
+               </div>
+                </div>
+            </div>
+
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">ID Sapi</th>
+                            <th scope="col">Status Transaksi</th>
+                            <th scope="col">Harga</th>
+                            <th scope="col">Tanggal Transaksi</th>
+                            <th scope="col">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($transaksi as $data)
+                    <tr>
+                            <td scope="row">{{ ++$i}}</td>
+                            <td>{{$data->sapi->nis}}</td>
+                            @if ($data->status_transaksi == 'Beli')
+                                <td class="text-center"><span class="badge badge-primary">Beli</span></td>
+                            @else
+                                <td class="text-center"><span class="badge badge-success">Jual</span></td>
+                            @endif
+                            <td>{{$data->harga}}</td>
+                            <td>{{$data->created_at}}</td>
+                            <td>
+                                <form action="{{ route('transaksi.destroy',  $data->id) }}" method="POST">
+                                    <a class="btn btn-icons btn-primary" href="{{route('transaksi.show', $data->id)}}"><i class="mdi mdi-eye"></i></a>
+                                    <a class="btn btn-icons btn-warning" href="{{route('transaksi.edit', $data->id)}}"><i class="mdi mdi-pencil"></i></a>
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-icons btn-danger"><i class="mdi mdi-delete"></i></button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="paginate">
+                <div class="container">
+                    <div class="row">
+                        <div class="detail-data col-md-12">
+                            <p>Page : {!! $transaksi->currentPage() !!} <br />
+                                Jumlah Data : {!! $transaksi->total() !!} <br />
+                                Data Per Halaman : {!! $transaksi->perPage() !!} <br />
+                            </p>
+                        </div>
+                        <div class="mx-auto">
+                            <div class="paginate-button col-md-12">
+                                {!! $transaksi->links('vendor.pagination.custom') !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+
