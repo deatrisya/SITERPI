9791d43dcdb99d8519d302af20ff3371d3451550
Ayi <ayianjani774@gmail.com>
2022-05-24 14:20:56 +0700

    update pakan seeder

--- database/seeders/PakanSeeder.php
+++ database/seeders/PakanSeeder.php
@@ -3,16 +3,32 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PakanSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     * reference : https://sumbarprov.go.id/home/news/12361-jenis-pakan-ternak-dan-kandungan-nutrisinya.html
      *
      * @return void
      */
     public function run()
     {
-        //
+        $dataItem = [
+            [
+                'jenis_pakan' => 'Pakan Kasar'
+            ],
+            [
+                'jenis_pakan' => 'Pakan Penguat'
+            ],
+            [
+                'jenis_pakan' => 'Pakan Fermentasi'
+            ],
+            [
+                'jenis_pakan' => 'Pakan Tambahan'
+            ]
+        ];
+        DB::table('pakans')->insert($dataItem);
     }
 }
