123d049057435dc5201336d6c40f4ac63ec4edb9
Ayi <ayianjani774@gmail.com>
2022-05-24 13:24:21 +0700

    membuat model RiwayatPakan

---
+++ app/Http/Controllers/RiwayatPakanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RiwayatPakan;
+use Illuminate\Http\Request;
+
+class RiwayatPakanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\RiwayatPakan  $riwayatPakan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RiwayatPakan $riwayatPakan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\RiwayatPakan  $riwayatPakan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RiwayatPakan $riwayatPakan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\RiwayatPakan  $riwayatPakan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RiwayatPakan $riwayatPakan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\RiwayatPakan  $riwayatPakan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RiwayatPakan $riwayatPakan)
+    {
+        //
+    }
+}
