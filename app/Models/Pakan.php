9f487b32579053308dbc4a94f90dfd67a97832e5
Ayi <ayianjani774@gmail.com>
2022-05-24 14:16:14 +0700

    Update Model

--- app/Models/Pakan.php
+++ app/Models/Pakan.php
@@ -3,9 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\RiwayatPakan;
 use Illuminate\Database\Eloquent\Model;
 
 class Pakan extends Model
 {
     use HasFactory;
+    protected $table ='pakans';
+    protected $primaryKey = 'id';
+
+    protected $fillable =[
+        'jenis_pakan'
+    ];
+
+    public function riwayatpakan(){
+        return $this->hasMany(RiwayatPakan::class);
+    }
 }
--- app/Models/RiwayatPakan.php
+++ app/Models/RiwayatPakan.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class RiwayatPakan extends Model
 {
     use HasFactory;
+    protected $table ='pakans';
+    protected $primaryKey = 'id';
+
+    protected $fillable =[
+        'jenis_pakan'
+    ];
+
+    public function riwayatpakan(){
+        return $this->hasMany(RiwayatPakan::class);
+    }
 }
