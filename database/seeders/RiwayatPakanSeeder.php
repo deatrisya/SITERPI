55dea44f84462aaa2eeedf359b942270a99b3699
deatrisya <mirelldee47@gmail.com>
2022-05-31 20:30:32 +0700

    Ganti waktu null dan harga

--- database/seeders/RiwayatPakanSeeder.php
+++ database/seeders/RiwayatPakanSeeder.php
@@ -19,21 +19,21 @@ class RiwayatPakanSeeder extends Seeder
                 'pakan_id' => 1,
                 'tanggal' => '2022-5-24',
                 'status' => 'Masuk',
-                'waktu' => 'Pagi',
+                'waktu' => null,
                 'jumlah' => 10,
-                'harga_satuan' => 100000,
+                'harga_satuan' => 10000,
                 'total_harga' => 100000
             ],
             [
                 'pakan_id' => 2,
                 'tanggal' => '2022-5-24',
                 'status' => 'Masuk',
-                'waktu' => 'Sore',
+                'waktu' => null,
                 'jumlah' => 10,
-                'harga_satuan' => 100000,
-                'total_harga' => 100000
+                'harga_satuan' => 20000,
+                'total_harga' => 200000
             ]
         ];
-        DB::table('riwayat_pakans')->insert($dataItem); 
+        DB::table('riwayat_pakans')->insert($dataItem);
     }
 }
