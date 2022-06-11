d9cdfb652f6fc48adbaa55de0538fb1ceac11369
deatrisya <mirelldee47@gmail.com>
2022-06-11 11:09:40 +0700

    add Dashboard function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Keuangan;
+use App\Models\Pegawai;
+use App\Models\Sapi;
+use App\Models\Transaksi;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,6 +29,35 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('index');
+        $today = Carbon::now()->format('l');
+        $date = Carbon::now()->format("d F, Y");
+
+        $pengeluaran = Keuangan::sum('keluar');
+        $transaksi = Transaksi::where('status_transaksi','Jual')->sum('harga');
+        $laba = $transaksi-$pengeluaran;
+        $pegawai = Pegawai::count();
+
+        $month =['1','2','3','4','5','6','7','8','9','10','11','12'];
+
+        foreach($month as $key => $value) {
+            $keuanganMasuk[] = Keuangan::whereMonth('created_at','=',$value)->sum('masuk');
+            $keuanganKeluar[] = Keuangan::whereMonth('created_at','=',$value)->sum('keluar');
+        }
+
+
+        return view('index',
+            ['pengeluaran'=>$pengeluaran,
+            'transaksi' => $transaksi,
+            'laba' => $laba,
+            'pegawai' => $pegawai,
+            'keuanganMasuk' => json_encode($keuanganMasuk,JSON_NUMERIC_CHECK),
+            'keuanganKeluar' => json_encode($keuanganKeluar,JSON_NUMERIC_CHECK),
+            'date' => $date,
+            'today' => $today,
+            ]
+        );
     }
+
+
+
 }
