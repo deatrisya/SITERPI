7d25c9c4e259085deee497c78d355e6f8fff2d22
deatrisya <mirelldee47@gmail.com>
2022-06-11 21:26:44 +0700

    add jenis sapi function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Jenis_Sapi;
 use App\Models\Keuangan;
 use App\Models\Pegawai;
 use App\Models\Sapi;
@@ -33,31 +34,44 @@ class HomeController extends Controller
         $date = Carbon::now()->format("d F, Y");
 
         $pengeluaran = Keuangan::sum('keluar');
-        $transaksi = Transaksi::where('status_transaksi','Jual')->sum('harga');
-        $laba = $transaksi-$pengeluaran;
+        $transaksi = Transaksi::where('status_transaksi', 'Jual')->sum('harga');
+        $laba = $transaksi - $pengeluaran;
         $pegawai = Pegawai::count();
 
-        $month =['1','2','3','4','5','6','7','8','9','10','11','12'];
 
-        foreach($month as $key => $value) {
-            $keuanganMasuk[] = Keuangan::whereMonth('created_at','=',$value)->sum('masuk');
-            $keuanganKeluar[] = Keuangan::whereMonth('created_at','=',$value)->sum('keluar');
+        $month = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+
+        foreach ($month as $key => $value) {
+            $keuanganMasuk[] = Keuangan::whereMonth('created_at', '=', $value)->sum('masuk');
+            $keuanganKeluar[] = Keuangan::whereMonth('created_at', '=', $value)->sum('keluar');
         }
 
-
-        return view('index',
-            ['pengeluaran'=>$pengeluaran,
+        $jenisSapi = [];
+        $dataJenis = Jenis_Sapi::all();
+        foreach ($dataJenis as $key => $value) {
+            $dataSapi = Sapi::where('jenissapi_id', $value->id)->where('status','Belum Terjual')->count();
+            array_push($jenisSapi,$dataSapi);
+        }
+        // dd($jenisSapi);
+        $data = [
+            'pengeluaran' => $pengeluaran,
             'transaksi' => $transaksi,
             'laba' => $laba,
             'pegawai' => $pegawai,
-            'keuanganMasuk' => json_encode($keuanganMasuk,JSON_NUMERIC_CHECK),
-            'keuanganKeluar' => json_encode($keuanganKeluar,JSON_NUMERIC_CHECK),
+            'keuanganMasuk' => json_encode($keuanganMasuk, JSON_NUMERIC_CHECK),
+            'keuanganKeluar' => json_encode($keuanganKeluar, JSON_NUMERIC_CHECK),
             'date' => $date,
             'today' => $today,
-            ]
+            'jenisSapi' => json_encode($jenisSapi, JSON_NUMERIC_CHECK),
+            'labelSapi' => Jenis_Sapi::pluck('jenis_sapi')->toArray(),
+        ];
+
+        // dd($data);÷
+
+        return view(
+            'index',
+
+            $data
         );
     }
-
-
-
 }
